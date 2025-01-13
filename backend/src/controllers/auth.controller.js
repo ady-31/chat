ad974557ba0186589574f3effbc794f7dfc11c69
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-01-13 23:48:28 +0530

    change

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -96,8 +96,8 @@ export const updateProfile = async(req, res) => {
         }
 
         const uploadResponse = await cloudinary.uploader(profilePic)
-        const updateUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new: true})
-        res.status(200).json(updateUser)
+        const updateUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true})
+        res.status(200).json(updatedUser)
     } catch (error) {
         console.log("Error in update profile", error);
         res.status(500).json({message: "Internal server error"});
