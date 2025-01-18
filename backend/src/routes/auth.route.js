5b73ca2106ddd21c11087d41029b4d07afe0b00b
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-01-19 02:42:07 +0530

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,18 +1,24 @@
-import express from "express"
-import { checkAuth,login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
-
+// Create a new express router
 const router = express.Router()
 
+// Route to handle user signup
 router.post("/signup", signup);
 
+// Route to handle user login
 router.post("/login", login);
 
+// Route to handle user logout
 router.post("/logout", logout);
 
+// Route to handle profile update, protected route
 router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, checkAuth)
+// Route to check user authentication status, protected route
+router.get("/check", protectRoute, checkAuth);
 
+// Export the router to be used in other parts of the application
 export default router;
