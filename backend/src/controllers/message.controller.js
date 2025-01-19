c084be5178f495d3580c864082152015b20d6552
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-01-19 22:06:56 +0530

    push

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import Message from "../models/message.model.js"
 
 export const getUsersForSidebar = async(req, res) =>{
     try {
@@ -14,5 +15,30 @@ export const getUsersForSidebar = async(req, res) =>{
 };
 
 export const getMessages = async(req, res) => {
-    
-}
+    try {
+        const { id:userToChatId } = req.params
+        const myId = req.user._id;
+
+        const message = await Message.find({
+            $or:[
+                {senderId:myId, recevierId:userToChatId},
+                {senderId:userToChatId, recevierId:myId},
+            ]
+        })
+
+        res.status(200).json(message)
+    } catch (error) {
+        console.log("Error is getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal server error"});  
+    }
+};
+
+
+export const sendMessage = async(req, res) =>{
+    try {
+        const { text, image } = req.body;
+        const { id: recevierId} = req.params;
+    } catch (error) {
+        
+    }
+};
