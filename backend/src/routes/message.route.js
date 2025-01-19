aa11ad1ca79464fa0511f8285dc8c4236ea6095c
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-01-19 21:53:28 +0530

    push

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,13 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware";
+import { getMessages, getUsersForSidebar } from "../controllers/message.controller";
 
 const router = express.express.Router();
 
 
 router.get("users", protectRoute, getUsersForSidebar)
+router.get("/:id", protectRoute, getMessages)
 
 export default router;
 
+ 
