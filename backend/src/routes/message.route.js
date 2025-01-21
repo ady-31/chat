f210e88890a886b5fac8bcb7211ef80a2c7ed1fd
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-01-21 23:04:02 +0530

    push

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,14 +1,11 @@
-import express from "express"
-import { protectRoute } from "../middleware/auth.middleware";
-import { getMessages, getUsersForSidebar } from "../controllers/message.controller";
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { sendMessage, getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
 
-const router = express.express.Router();
+const router = express.Router();
 
-
-router.get("users", protectRoute, getUsersForSidebar);
+router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
-router.post("/send/:id", protectRoute, sendMessage)
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
-
- 
