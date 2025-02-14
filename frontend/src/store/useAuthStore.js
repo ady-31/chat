06053bb3736841927295f71f88a03919e286ac04
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-15 02:45:23 +0530

    psh

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -15,7 +15,7 @@ export const useAuthStore = create((set) => ({
 
             set({authUser:res.data})
         } catch (error){
-            cons
+            console.log("Error in checkAuth", error);
             set({authUser:res.data})
 
         } finally{
