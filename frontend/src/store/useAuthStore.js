7dbc46b7d4ed1a65a8532cc0633167bcbdee66a3
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-15 02:43:40 +0530

    push

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,10 +1,22 @@
 import {create} from "zustand"
+import { axiosInstance } from "../lib/axios.js";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
-    isSigningup
+    isSigningUp: false,
+    isLoggingIng: false,
+    isUpdatingProfile: false,
 
     isCheckinAuth: true,
-    
 
-}))
+    checkAuth: async() => {
+        try{
+            const res = await axiosInstance.get("/auth/check");
+
+            set({authUser:res.data})
+        } catch (error){
+            set({authUser:res.data})
+        }
+    },
+
+}));
