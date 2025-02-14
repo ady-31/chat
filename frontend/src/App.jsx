12a6c9de4c829ade12fa58d94f3ccf14e9b90323
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-14 22:52:14 +0530

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,10 @@ return(
 
 <Routes>
   <Route path="/" element={<HomePage />} />
-  <Route path="/" element={<HomePage />} />
-  <Route path="/" element={<HomePage />} />
-  <Route path="/" element={<HomePage />} />
-  <Route path="/" element={<HomePage />} />
+  <Route path="/signup" element={<SignUpPage />} />
+  <Route path="/login" element={<LoginPage />} />
+  <Route path="/settings" element={<SettingsPage />} />
+  <Route path="/profile" element={<ProfilePage />} />
 
 </Routes>
 
