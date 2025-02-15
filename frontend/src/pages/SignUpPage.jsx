d57e153313a6870b0f291d7569e75adeeffc3360
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-16 01:42:11 +0530

    psh

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,6 +3,6 @@ import { useState } from "react";
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   return (<div>SignUpPage</div>)
-}
+};
 
 export default SignUpPage;
