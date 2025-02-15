1a9ffbcab4a643064189964b7935598a8284be51
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-16 01:34:42 +0530

    sph

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,7 +37,7 @@ return(
   <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
   <Route path="/signup" element={<SignUpPage />} />
   <Route path="/login" element={<LoginPage />} />
-  <Route path="/settings" element={authUser ? <SettingsPage /> : <Navigate to="/login" />} />
+  <Route path="/settings" element={<SettingsPage />} />
   <Route path="/profile" element={<ProfilePage />} />
 </Routes>
 
