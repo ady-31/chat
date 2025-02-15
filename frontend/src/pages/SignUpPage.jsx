2b552b2ddf77daa3866a8d03e7d13d7723345a9b
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-16 01:55:17 +0530

    psh

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -20,7 +20,8 @@ const SignUpPage = () => {
   return (<div className="min-h-screen grid lg:grid-cols-2">
     { /*left wali side*/}
     <div className="flex flex-col justify-center items-center p-6 sm:p-12">
-      
+      <div className="w-full max-w-md space-y-8">
+        <div/>
     </div>
   </div>)
 };
