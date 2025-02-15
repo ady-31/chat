924819ef21e477e3be5dc6a66b5db831d9c0b310
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-16 01:21:56 +0530

    psh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,13 +12,19 @@ import { useEffect } from "react";
 
 
 const App = () => {
-  const {authUser, checkAuth} = useAuthStore()
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
   console.log({ authUser});
+
+  if(isCheckingAuth && !authUser) return{
+    <div className="flex items-center justify-center h-screen">
+    <Loader className="size-10 animate-spin"/>
+    </div>
+  }
 return(
   <div>
 
