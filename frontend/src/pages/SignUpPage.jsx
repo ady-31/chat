c39de85848e11a67cafe9e902856f91407bcd978
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-16 01:57:40 +0530

    psh

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -17,13 +17,16 @@ const SignUpPage = () => {
     e.preventDefault()
   }
 
-  return (<div className="min-h-screen grid lg:grid-cols-2">
+  return <div className="min-h-screen grid lg:grid-cols-2">
     { /*left wali side*/}
     <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+
       <div className="w-full max-w-md space-y-8">
+        <div className="flex flex-col items-center gap-2 group">
+          <div/>
         <div/>
     </div>
-  </div>)
+  </div>
 };
 
 export default SignUpPage;
