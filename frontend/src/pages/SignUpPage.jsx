8d299789965a378c4dc59abe7007d31691787fc2
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-16 01:59:37 +0530

    psh

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -23,6 +23,8 @@ const SignUpPage = () => {
 
       <div className="w-full max-w-md space-y-8">
         <div className="flex flex-col items-center gap-2 group">
+          <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+            <div/>
           <div/>
         <div/>
     </div>
