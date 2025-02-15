3f9a7017689b3dca7e256f55d23a369b3a11ef3e
Aman Deep Yadav <107334593+ady-31@users.noreply.github.com>
2025-02-16 02:19:16 +0530

    psh

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { MessageSquare } from "lucide-react";
+import { MessageSquare, User } from "lucide-react";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -37,24 +37,27 @@ const SignUpPage = () => {
             </p>
           </div>
 
+          {/* Form */}
           <form onSubmit={handleSubmit} className="space-y-6">
             <div className="form-control">
-              <label className="lable">
-                <span className="leble-text font-medium">Full Name</span>
+              <label className="label">
+                <span className="label-text font-medium">Full Name</span>
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <User className="size-5 text-base-content/40"/>
+                  <User className="size-5 text-base-content/40" />
                 </div>
                 <input
-                type="text"
-                className={`input input-bordered w-full pl-10`}
-                placeholder="mj"
-                value={FormDataEvent.fullName}
-                onChange={(e) => setFormData({...formData, fullName: e.target.value })}
+                  type="text"
+                  className="input input-bordered w-full pl-10"
+                  placeholder="mj"
+                  value={formData.fullName}
+                  onChange={(e) =>
+                    setFormData({ ...formData, fullName: e.target.value })
+                  }
                 />
               </div>
-              <div/>
+            </div>
           </form>
         </div>
       </div>
